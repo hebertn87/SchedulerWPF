bf6fa8662bec483294b3cc59c49297964c76aa70
Nate Hebert <hebertn87@gmail.com>
2018-10-15 21:59:23 -0600

    Pushed up new working version of code. Code will run until it hits converter. Overhauled most of the VM and Model to make validation work. Created a command class that takes an icommand.

---
+++ Scheduler/Command/CreateToDoCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Scheduler.Model;
+using Scheduler.ViewModel;
+
+namespace Scheduler.Command
+{
+    class CreateToDoCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private ToDo todo = new ToDo();
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(Object parameter)
+        {
+            if (parameter is ToDoListVM toDoList)
+            {
+                toDoList.ToDoItem.Add(new ToDoListVM() {Name = toDoList.Name, Desc = toDoList.Desc, DoneBy = toDoList.DoneBy, IsDone = false });
+            }
+        }
+    }
+}
---
+++ Scheduler/ViewModel/ToDoListVM.cs
@@ -0,0 +1,63 @@
+﻿using Scheduler.Command;
+using Scheduler.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Scheduler.ViewModel
+{
+    class ToDoListVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<ToDoListVM> todo = new ObservableCollection<ToDoListVM>();
+
+        public ObservableCollection<ToDoListVM> ToDoItem {
+            get { return todo; } 
+            set
+            {
+                if (todo != value)
+                {
+                    todo = value;
+                    NotifyPropertyChanged(nameof(ToDoItem));
+                }
+            }
+        }
+
+        public ICommand CreateToDoCommand { get { return new CreateToDoCommand(); } }
+
+        public DateTime DoneBy { get; set; }
+        public string Desc { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    //Custom class that implements the Ivalue Converter
+    public class DateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String dateTime = (String)value.ToString();
+
+            return dateTime;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
